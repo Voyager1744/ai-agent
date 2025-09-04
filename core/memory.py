8df0ba97eddafb8d714105d67c6156398a44cf96
Dmitriy Ushakov Air <udv8@ya.ru>
2025-09-04 22:57:26 +0300

    add memory

---
+++ core/memory.py
@@ -0,0 +1,12 @@
+class Memory:
+    def __init__(self, max_messages: int = 10):
+        self.messages: list[dict] = []
+        self.max_messages = max_messages
+
+    def add(self, role: str, content: str):
+        self.messages.append({'role': role, 'content': content})
+        if len(self.messages) > self.max_messages:
+            self.messages = self.messages[-self.max_messages:]
+
+    def get(self):
+        return self.messages
